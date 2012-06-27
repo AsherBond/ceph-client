1844c455fd05e4fc0522439dbef70f84b86b5146
Sage Weil <sage@inktank.com>
2012-06-27 12:24:34 -0700

    libceph: initialize mon_client con only once
    Do not re-initialize the con on every connection attempt.  When we
    ceph_con_close, there may still be work queued on the socket (e.g., to
    close it), and re-initializing will clobber the work_struct state.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- net/ceph/mon_client.c
+++ net/ceph/mon_client.c
@@ -142,9 +142,6 @@ static int __open_session(struct ceph_mon_client *monc)
 		monc->sub_renew_after = jiffies;  /* i.e., expired */
 		monc->want_next_osdmap = !!monc->want_next_osdmap;
 
-		ceph_con_init(&monc->con, monc, &mon_con_ops,
-			&monc->client->msgr);
-
 		dout("open_session mon%d opening\n", monc->cur_mon);
 		ceph_con_open(&monc->con,
 			      CEPH_ENTITY_TYPE_MON, monc->cur_mon,
@@ -797,6 +794,9 @@ int ceph_monc_init(struct ceph_mon_client *monc, struct ceph_client *cl)
 	monc->pending_auth = 0;
 	if (!monc->m_auth)
 		goto out_auth_reply;
+
+	ceph_con_init(&monc->con, monc, &mon_con_ops,
+		      &monc->client->msgr);
 
 	monc->cur_mon = -1;
 	monc->hunting = true;
